579e9424f7ba1186118edeced7af74e34830be4b
carls0n <138807964+carls0n@users.noreply.github.com>
2023-11-03 14:31:22 -0700

    Add files via upload

--- change-theme.sh
+++ change-theme.sh
@@ -5,13 +5,28 @@
 [[ `uname -s` == "OpenBSD" ]] && path=/usr/local/share/
 [[ `uname -s` == "Linux" ]] && path=/usr/share/
 
-if [[ -e ~/.themes/$1/xfwm4 ]] || [[ -e $path/themes/$1/xfwm4 ]]
+if [[ $1 != "ls" ]] && [[ -e ~/.themes/$1/xfwm4 ]] || [[ -e $path/themes/$1/xfwm4 ]]
 then
 xfconf-query -c xfwm4 -p /general/theme -s $1
-xfconf-query -c xsettings -p /Net/ThemeName -t "string" -s $1 --create
-else echo "No theme by that name. Check capitalization."
 fi
+
+if [[ $1 != "ls" ]] && [[ -e ~/.themes/$1/gtk-3.0 ]] || [[ -e $path/themes/$1/gtk-3.0 ]]
+then
+xfconf-query -c xsettings -p /Net/ThemeName -t "string" -s $1 --create
+elif [[ $1 != "ls" ]] && [[ ! -e ~/.themes/$1/gtk-3.0 ]] || [[ -e $path/themes/$1/gtk-3.0 ]]
+then
+echo "No theme by that name. Check capitalization."
+fi
+
 if [[ -e ~/.themes/$1/xfce-notify-4.0 ]] || [[ -e $path/themes/$1/xfce-notify-4.0 ]]
 then
 xfconf-query -c xfce4-notifyd -p /theme -t "string" -s $1
+fi
+
+if [[ $1 == "ls" ]]
+then
+for file in {~/.themes/*/gtk-3.0/gtk.css,$path/themes/*/gtk-3.0/gtk.css}
+do
+ls "$file" | awk -F "/" '{print $(NF - 2)}' 
+done
 fi
